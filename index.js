01dd8fb97c1129ac99c9d593876d008a765e962b
Tim Potts <timothy.potts@colorado.edu>
2019-11-10 18:02:34 -0700

    have a basic html shell to test. Will see if it works

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
+
+function Profile(name, bio, img, repos, stars, followers, following){
+  this.name = name;
+  this.bio = bio;
+  this.image = img;
+  this.repos = repos;
+  this.stars = stars;
+  this.followers = followers;
+  this.following = following;
+}
+
+
+
+
+
 inquirer
   .prompt({
     message: "Enter your GitHub username:",
@@ -10,20 +25,17 @@ inquirer
   .then(function({ username }) {
     const queryUrl = `https://api.github.com/users/${username}`;
 
-    axios.get(queryUrl).then(function(res) {
-      const repoNames = res.data.map(function(repo) {
-        return repo.name;
-      });
-
-      const repoNamesStr = repoNames.join("\n");
-
-      fs.writeFile("repos.txt", repoNamesStr, function(err) {
-        if (err) {
-          throw err;
-        }
-
-        console.log(`Saved ${repoNames.length} repos`);
-      });
+    axios.get(queryUrl).then(function(response) {
+      data = response.data;
+      name = data.login;
+      bio = data.bio;
+      img = data.avatar_url;
+      repos = data.public_repos;
+      stars = data.starred_url.length;
+      followers = data.followers;
+      following = data.following;
+      generateHTML(name, bio, img, repos, followers, following)
+      console.log(firstObject);
     });
   });
 
@@ -56,179 +68,50 @@ const colors = {
     }
   };
   
-  function generateHTML(data) {
+  function generateHTML(name, bio, img, repos, followers, following) {
     return `<!DOCTYPE html>
-  <html lang="en">
-     <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-        <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-        <title>Document</title>
-        <style>
-            @page {
-              margin: 0;
-            }
-           *,
-           *::after,
-           *::before {
-           box-sizing: border-box;
-           }
-           html, body {
-           padding: 0;
-           margin: 0;
-           }
-           html, body, .wrapper {
-           height: 100%;
-           }
-           .wrapper {
-           background-color: ${colors[data.color].wrapperBackground};
-           padding-top: 100px;
-           }
-           body {
-           background-color: white;
-           -webkit-print-color-adjust: exact !important;
-           font-family: 'Cabin', sans-serif;
-           }
-           main {
-           background-color: #E9EDEE;
-           height: auto;
-           padding-top: 30px;
-           }
-           h1, h2, h3, h4, h5, h6 {
-           font-family: 'BioRhyme', serif;
-           margin: 0;
-           }
-           h1 {
-           font-size: 3em;
-           }
-           h2 {
-           font-size: 2.5em;
-           }
-           h3 {
-           font-size: 2em;
-           }
-           h4 {
-           font-size: 1.5em;
-           }
-           h5 {
-           font-size: 1.3em;
-           }
-           h6 {
-           font-size: 1.2em;
-           }
-           .photo-header {
-           position: relative;
-           margin: 0 auto;
-           margin-bottom: -50px;
-           display: flex;
-           justify-content: center;
-           flex-wrap: wrap;
-           background-color: ${colors[data.color].headerBackground};
-           color: ${colors[data.color].headerColor};
-           padding: 10px;
-           width: 95%;
-           border-radius: 6px;
-           }
-           .photo-header img {
-           width: 250px;
-           height: 250px;
-           border-radius: 50%;
-           object-fit: cover;
-           margin-top: -75px;
-           border: 6px solid ${colors[data.color].photoBorderColor};
-           box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-           }
-           .photo-header h1, .photo-header h2 {
-           width: 100%;
-           text-align: center;
-           }
-           .photo-header h1 {
-           margin-top: 10px;
-           }
-           .links-nav {
-           width: 100%;
-           text-align: center;
-           padding: 20px 0;
-           font-size: 1.1em;
-           }
-           .nav-link {
-           display: inline-block;
-           margin: 5px 10px;
-           }
-           .workExp-date {
-           font-style: italic;
-           font-size: .7em;
-           text-align: right;
-           margin-top: 10px;
-           }
-           .container {
-           padding: 50px;
-           padding-left: 100px;
-           padding-right: 100px;
-           }
-  
-           .row {
-             display: flex;
-             flex-wrap: wrap;
-             justify-content: space-between;
-             margin-top: 20px;
-             margin-bottom: 20px;
-           }
-  
-           .card {
-             padding: 20px;
-             border-radius: 6px;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
-             margin: 20px;
-           }
-           
-           .col {
-           flex: 1;
-           text-align: center;
-           }
-  
-           a, a:hover {
-           text-decoration: none;
-           color: inherit;
-           font-weight: bold;
-           }
-  
-           @media print { 
-            body { 
-              zoom: .75; 
-            } 
-           }
-        </style>`
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Portfolio</title>
+    </head>
+    
+    <body>
+        Copy
+        <div class="jumbotron">
+          <h1 class="display-4">${name}</h1>
+          <img src=${img}>
+          <p class="lead">${bio}</p>
+          <p class="lead">${followers}</p>
+          <p class="lead">${following}</p>
+          <p class="lead">${repos}</p>
+          <p class="lead">${stars}</p>
+          <p class="lead"></p>
+          <hr class="my-4">
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <a href="#" role="button">Learn more</a>
+        </div>
+    
+    
+    
+    
+    
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossorigin="anonymous"></script>
+    </body>
+    
+    </html>`
           }
   
-
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "username",
-      message: "What is your username?"
-    },
-
-    {
-      type: "input",
-      name: "location",
-      message: "Where are you from?"
-    },
-
-    {
-      type: "input",
-      name: "github",
-      message: "Enter your GitHub Username"
-    },
-
-    {
-      type: "input",
-      name: "linkedin",
-      message: "Enter your LinkedIn URL."
-    }
-  ]);
-}
